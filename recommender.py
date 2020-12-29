73b7280db24b5596104b4a855f08ffec2724227e
Prashant Mudgal <Prashantmdgl9@users.noreply.github.com>
2020-12-29 18:37:43 +0530

    Update recommender.py

--- recommender.py
+++ recommender.py
@@ -4,7 +4,7 @@ uri = "neo4j://localhost:7687"
 user = "neo4j"
 password = "hello@123"
 driver = GraphDatabase.driver(uri, auth=(user, password))
-
+session = driver.session(
 
 
 def recommendations(service = '', location = '', client = ''):
